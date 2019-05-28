cf2d4107699ecffcbffebd5ee78206c07e526850
Stoabrogga <38475780+Stoabrogga@users.noreply.github.com>
2019-05-28 22:17:06 +0000

    feat(DB): add DB files to db_assembler

--- sql/characters/custom_item_enchant_visuals.sql
+++ sql/characters/custom_item_enchant_visuals.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `custom_item_enchant_visuals` (
+CREATE TABLE IF NOT EXISTS `custom_item_enchant_visuals` (
         `iguid` INT(10) UNSIGNED NOT NULL COMMENT 'item DB guid',
         `display` INT(10) UNSIGNED NOT NULL COMMENT 'enchantID',
-		`PlayerName` varchar(50) NOT NULL,
+        `PlayerName` varchar(50) NOT NULL,
         PRIMARY KEY(`iguid`)
         )
         COMMENT = 'stores the enchant IDs for the visuals'
-        COLLATE = 'latin1_swedish_ci'
+        DEFAULT CHARSET = utf8
         ENGINE = InnoDB;
