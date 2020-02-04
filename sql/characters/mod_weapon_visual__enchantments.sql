5c830fca9b1c3112b148597bf0e78d9b1126caac
BarbzYHOOL <professor_barbz@gmx.com>
2020-02-04 22:49:27 +0100

    refactor: Rename table and columns

--- sql/characters/mod_weapon_visual__enchantments.sql
+++ sql/characters/mod_weapon_visual__enchantments.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS `custom_item_enchant_visuals` (
-    `iguid` INT(10) UNSIGNED NOT NULL COMMENT 'item DB guid',
+CREATE TABLE IF NOT EXISTS `mod_weapon_visual__enchantments` (
+    `item_guid` INT(10) UNSIGNED NOT NULL COMMENT 'item DB guid',
     `display` INT(10) UNSIGNED NOT NULL COMMENT 'enchantID',
     `PlayerName` varchar(50) NOT NULL,
-    PRIMARY KEY(`iguid`))
+    PRIMARY KEY(`item_guid`))
     COMMENT = 'Stores the enchant IDs for the visuals of mod-weapon-visual'
     DEFAULT CHARSET = utf8
     ENGINE = InnoDB;
---
+++ sql/characters/mod_weapon_visual_enchantments.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `mod_weapon_visual_enchantments` (
+    `item_guid` INT(10) UNSIGNED NOT NULL COMMENT 'item DB guid',
+    `display` INT(10) UNSIGNED NOT NULL COMMENT 'enchantID',
+    PRIMARY KEY(`item_guid`))
+    COMMENT = 'Stores the enchant IDs for the visuals of mod-weapon-visual'
+    DEFAULT CHARSET = utf8
+    ENGINE = InnoDB;
