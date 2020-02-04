cad617e22ee84024661bcaca1ab236e08fe437f5
BarbzYHOOL <professor_barbz@gmx.com>
2020-02-04 22:33:01 +0100

    chore: Rename/move the SQL files

--- sql/characters/custom_item_enchant_visuals.sql
+++ sql/characters/custom_item_enchant_visuals.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS `custom_item_enchant_visuals` (
-        `iguid` INT(10) UNSIGNED NOT NULL COMMENT 'item DB guid',
-        `display` INT(10) UNSIGNED NOT NULL COMMENT 'enchantID',
-        `PlayerName` varchar(50) NOT NULL,
-        PRIMARY KEY(`iguid`)
-        )
-        COMMENT = 'stores the enchant IDs for the visuals'
-        DEFAULT CHARSET = utf8
-        ENGINE = InnoDB;
+    `iguid` INT(10) UNSIGNED NOT NULL COMMENT 'item DB guid',
+    `display` INT(10) UNSIGNED NOT NULL COMMENT 'enchantID',
+    `PlayerName` varchar(50) NOT NULL,
+    PRIMARY KEY(`iguid`))
+    COMMENT = 'Stores the enchant IDs for the visuals of mod-weapon-visual'
+    DEFAULT CHARSET = utf8
+    ENGINE = InnoDB;
