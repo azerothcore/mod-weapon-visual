cc327adb467c49c7c84c9facadf5c0d2b2d9af2e
BarbzYHOOL <professor_barbz@gmx.com>
2020-02-04 22:49:55 +0100

    fix(sql): Do not store playername as it's useless

--- sql/characters/mod_weapon_visual__enchantments.sql
+++ sql/characters/mod_weapon_visual__enchantments.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS `mod_weapon_visual__enchantments` (
     `item_guid` INT(10) UNSIGNED NOT NULL COMMENT 'item DB guid',
     `display` INT(10) UNSIGNED NOT NULL COMMENT 'enchantID',
-    `PlayerName` varchar(50) NOT NULL,
     PRIMARY KEY(`item_guid`))
     COMMENT = 'Stores the enchant IDs for the visuals of mod-weapon-visual'
     DEFAULT CHARSET = utf8
